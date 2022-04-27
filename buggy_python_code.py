7342026eea07a777c0da80e98321c85ecb11f033
unknown <xnovotn6@ad.fi.muni.cz>
2022-04-27 18:36:29 +0200

    [Fix] Formating, unused values, renamed function

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,19 +1,18 @@
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
-import subprocess
 import base64
 import subprocess
 import flask
 
+
 # Input injection
-def transcode_file(request, filename):
+def transcode_file(filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
+def something_else(user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
@@ -21,10 +20,12 @@ def foo(request, user):
 # Pickles
 class RunBinSh(object):
     def __reduce__(self):
-        return (subprocess.Popen, (('/bin/sh',),))
+        return subprocess.Popen, (('/bin/sh',),)
+
 
 def import_urlib_version(version):
     exec("import urllib%s as urllib" % version)
+
 
 @app.route('/')
 def index():
